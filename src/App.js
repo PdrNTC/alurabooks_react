8625948b62446968766cc45a398eaf772c467b84
Pedro Henrique Ernandes da Silva <thepedrinhoo@gmail.com>
2024-08-14 16:36:49 -0300

    Componente input e pesquisa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './componentes/Header';
+import Pesquisa from './componentes/Pesquisa';
 import styled from 'styled-components';
 
 // Criando um componente estilizado direto sem precisar de CSS //
@@ -6,16 +7,13 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002F52 35%, #326589);
-
-  li {
-    list-style: none;
-  }
 `
 
 function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa />
     </AppContainer>
   );
 }
